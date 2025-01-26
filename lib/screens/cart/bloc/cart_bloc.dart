2cb489332fdbb4715800f49cf78a56966e114744
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2025-01-26 14:12:08 +0330

    create checkout screen

--- lib/screens/cart/bloc/cart_bloc.dart
+++ lib/screens/cart/bloc/cart_bloc.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
@@ -7,7 +5,7 @@ import 'package:flutter/cupertino.dart';
 import '../../../common/exceptions.dart';
 import '../../../data/model/auth_info.dart';
 import '../../../data/model/cart_response.dart';
-import '../../../data/model/order.dart';
+
 import '../../../data/repo/cart_repository.dart';
 
 part 'cart_event.dart';
